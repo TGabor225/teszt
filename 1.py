09019ca4316026fdf96d982eeef212e439dd3dee
TGabor225 <79319483+TGabor225@users.noreply.github.com>
2021-11-17 10:25:36 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+#Ez egy program ami eldönti a számról ,
+print("Kérek egy számot")
+szam= -5
+if(szam>0):
+    print('poz')
+    elif(szam == 0): 
+    print(0)
+    else: 
+        print('neg')
+
+
+    
